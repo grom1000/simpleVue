6864fc29fba8251fb31044918e0d105492382a15
Nikolay <ntamarin27@gmail.com>
2020-02-20 17:09:10 +0300

    2 components

---
+++ src/components/aa.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img class="rounded" :src="img" :alt="imgAlt" />
+    <div class="mt-2">
+      <div>
+        <div class="text-xs text-gray-600 uppercase font-bold">{{ eyebrow }}</div>
+        <div class="font-bold text-gray-700 leading-snug">
+          <a :href="url" class="hover:underline">{{ title }}</a>
+        </div>
+        <div class="mt-2 text-sm text-gray-600">{{ pricing }}</div>
+      </div>
+    </div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'aa',
+  props: ["img", "imgAlt", "eyebrow", "title", "pricing", "url", 'message']
+};
+</script>
---
+++ src/components/bb.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      img="/img/cancun.jpg"
+      imgAlt="Beach in Cancun"
+      eyebrow="Private Villa"
+      title="Relaxing All-Inclusive Resort in Cancun"
+      pricing="$299 USD per night"
+      url="/vacations/cancun"
+    </div>
+    <component-a message="message0000000000000"></component-a>
+  </div>
+</template>
+
+<script>
+import aa from './aa.vue'
+
+export default {
+  name: 'bb',
+  data: function () {
+    return {
+      message: 'Hello'
+    }
+  },
+  components: {
+    'component-a': aa
+  }
+}
+</script>
